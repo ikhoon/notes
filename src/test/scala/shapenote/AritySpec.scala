1d4ac0b19ff7bf2e5f10fa5ac7b7a15a37183643
Ikhoon Eom <ih.pert@gmail.com>
2016-08-14 11:57:11 +0900

    Arity

---
+++ src/test/scala/shapenote/AritySpec.scala
@@ -0,0 +1,26 @@
+package shapenote
+
+import org.scalatest.{Matchers, WordSpec}
+import shapeless.{Generic, HList}
+
+/**
+  * Created by ikhoon on 2016. 8. 14..
+  */
+class AritySpec extends WordSpec with Matchers {
+
+  "Facility for abstracting over arity" should {
+
+    "arity" in {
+      import shapeless.syntax.std.function._
+      import shapeless.ops.function._
+
+      def applyProduct[P <: Product, F, L <: HList, R]
+        (p: P)(f : F)(implicit gen: Generic.Aux[P, L], fp: FnToProduct.Aux[F, L => R]) =
+        f.toProduct(gen.to(p))
+
+      applyProduct(1, 2)((_: Int) + (_: Int)) shouldBe 3
+      applyProduct(1, 2, 3)((_: Int) * (_: Int) * (_: Int)) shouldBe 6
+    }
+  }
+
+}
