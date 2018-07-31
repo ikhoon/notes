15daf9927f90864104e8962d082084641eaf7316
Ikhoon Eom <ih.pert@gmail.com>
2018-07-31 23:41:04 +0900

    solve coursera algorithm - strong connected components

--- scala-exercise/src/main/scala/algonote/Algo_2_1_StrongConnectedComponents.scala
+++ scala-exercise/src/main/scala/algonote/Algo_2_1_StrongConnectedComponents.scala
@@ -1,5 +1,10 @@
 package algonote
 
+import pprint.Tree.Ctx
+
+import scala.annotation.tailrec
+import scala.collection.mutable
+
 /**
   * Created by ikhoon on 30/07/2018.
   */
@@ -13,53 +18,110 @@ object Algo_2_1_StrongConnectedComponents {
       .map(_.split("\\s+"))
       .map(_.map(_.toInt).toList)
       .map { case x :: y :: Nil => (x, y) }
-      .foldLeft(Map.empty: Graph) { case (acc, (vertex, edge)) =>
-         modify[Int, Vector[Int]](acc, vertex, Vector.empty, _ :+ edge)
+      .foldLeft(Map.empty: Graph) {
+        case (acc, (vertex, edge)) =>
+          modify[Int, Vector[Int]](acc, vertex, Vector.empty, _ :+ edge)
       }
   }
 
   def rev(graph: Graph): Graph = {
-    graph.foldLeft(Map.empty: Graph) { case (acc, (vertex, edges)) =>
-        edges.foldLeft(acc) { case (newGraph, edge) =>
-          modify[Int, Vector[Int]](newGraph, edge, Vector.empty, _ :+ vertex)
+    graph.foldLeft(Map.empty: Graph) {
+      case (acc, (vertex, edges)) =>
+        edges.foldLeft(acc) {
+          case (newGraph, edge) =>
+            modify[Int, Vector[Int]](newGraph, edge, Vector.empty, _ :+ vertex)
         }
     }
   }
 
-  def dfsLoop(graph: Graph) = {
-    var explored = Set.empty[Int]
-    var leader = Map.empty[Int, Int]
-    var f = Map.empty[Int, Int]
-    def isExplored(node: Int): Boolean = explored.contains(node)
+  case class SCCContext(i: Int,
+                        s: Int,
+                        explored: mutable.Set[Int],
+                        leader: mutable.Map[Int, Int],
+                        f: Map[Int, Int],
+                        t: Int)
 
-    var t = 0
-    val keys: Vector[Int] = graph.keys.toVector
-    def dfs(node: Int, start: Int): () = {
-      explored += node
-      leader += node -> start
-      graph(node).foreach(edge => {
-        if(!isExplored(edge)) {
-          dfs(edge, start)
-        }
-      })
-      t += 1
-      f += node -> t
+  def dfs(graph: Graph, ctx: SCCContext): SCCContext = {
+    ctx.explored += ctx.i
+    ctx.leader += (ctx.i -> ctx.s)
+    var marked = ctx
+//    var marked = ctx.copy(explored = ctx.explored + ctx.i,
+//                          leader = ctx.leaderupdated(ctx.i, ctx.s))
+
+    var adj = graph.getOrElse(marked.i, Vector.empty)
+    while (adj.nonEmpty) {
+      val j = adj.head
+      if (!marked.explored.contains(j)) {
+        marked = dfs(graph, marked.copy(i = j))
+      }
+      adj = adj.tail
     }
 
+    val incT = marked.t + 1
+    marked.copy(t = incT, f = marked.f + (ctx.i -> incT))
+
+    /**
+    val traversed = graph.getOrElse(marked.i, Vector.empty).foldLeft(marked) {
+      case (curr, j) =>
+        if (!isExplored(j, curr.explored)) dfs(graph, curr.copy(i = j))
+        else curr
+    }
+
+    val incT = traversed.t + 1
+    traversed.copy(t = incT, f = traversed.f + (ctx.i -> incT))
+    */
+  }
+
+  def isExplored(node: Int, explored: Set[Int]): Boolean =
+    explored.contains(node)
+
+  def dfsLoop(graph: Graph): SCCContext = {
+
+    val zero =
+      SCCContext(0, 0, mutable.Set.empty, mutable.Map.empty, Map.empty, 0)
+
+    val keys: Vector[Int] = graph.keys.toVector
     keys
       .sortWith(_ > _)
-      .foreach(node => {
-        if(!isExplored(node)) {
-          dfs(node, node)
-        }
-      })
-    (leader, f)
+      .foldLeft(zero) {
+        case (ctx, i) =>
+          if (!ctx.explored.contains(i)) dfs(graph, ctx.copy(i = i, s = i))
+          else ctx
+      }
   }
-
 
   def modify[K, V](map: Map[K, V], key: K, default: V, f: V => V): Map[K, V] = {
     val value = map.getOrElse(key, default)
     map.updated(key, f(value))
   }
 
+  def dfsStrongComponent(graph: Graph,
+                         start: Int,
+                         node: Int,
+                         explored: Set[Int],
+  ): Vector[Int] = {
+
+    def dfs0(graph: Graph,
+             start: Int,
+             node: Int,
+             explored: Set[Int],
+             component: Vector[Int]): (Set[Int], Vector[Int]) = {
+      if (node % 100 == 0)
+        println(s"start = ${start}, node = $node, explored = ${explored.size}")
+      if (explored.contains(node)) (explored, component)
+      else {
+        val newExplored: Set[Int] = explored + node
+        val newComponent: Vector[Int] = component :+ node
+        graph
+          .getOrElse(node, Vector.empty)
+          .foldLeft((newExplored, newComponent)) {
+            case ((currExp: Set[Int], currComp: Vector[Int]), i) =>
+              if (currExp.contains(i)) (currExp, currComp)
+              else dfs0(graph, start, i, currExp, currComp)
+          }
+      }
+    }
+
+    dfs0(graph, start, node, explored, Vector.empty)._2
+  }
 }
--- scala-exercise/src/test/scala/algonote/Algo_2_1_StrongConnectedComponentsTest.scala
+++ scala-exercise/src/test/scala/algonote/Algo_2_1_StrongConnectedComponentsTest.scala
@@ -31,7 +31,6 @@ class Algo_2_1_StrongConnectedComponentsTest extends FunSuite with Matchers {
     rev(graph) shouldBe revervsed
   }
 
-
   test("build") {
     val graph: Graph = Map(
       1 -> Vector(2, 3, 4),
@@ -50,6 +49,109 @@ class Algo_2_1_StrongConnectedComponentsTest extends FunSuite with Matchers {
   }
 
   test("dfsLoop") {
-    
+    val graph: Graph = Map(
+      1 -> Vector(2, 3, 4),
+      2 -> Vector(4, 5),
+      3 -> Vector(4)
+    )
+    val ctx = dfsLoop(graph)
+    println(s"leader = ${ctx.leader}")
+    println(s"f = ${ctx.f}")
+  }
+
+  test("simple") {
+    val inputs =
+      """
+        |1 4
+        |7 1
+        |4 7
+        |
+        |9 7
+        |
+        |9 3
+        |6 9
+        |3 6
+        |
+        |8 6
+        |
+        |2 8
+        |8 5
+        |5 2
+      """.stripMargin
+    val graph = build(inputs.split("\n"))
+    val ctx = dfsLoop(rev(graph))
+    ctx.leader shouldBe Map(5 -> 9,
+                            1 -> 7,
+                            6 -> 9,
+                            9 -> 9,
+                            2 -> 9,
+                            7 -> 7,
+                            3 -> 9,
+                            8 -> 9,
+                            4 -> 7)
+    ctx.f shouldBe Map(5 -> 2,
+                       1 -> 7,
+                       6 -> 5,
+                       9 -> 6,
+                       2 -> 3,
+                       7 -> 9,
+                       3 -> 1,
+                       8 -> 4,
+                       4 -> 8)
+
+    val fReversed: Vector[(Int, Int)] =
+      ctx.f.view.map { case (k, v) => v -> k }.toVector.sortWith {
+        case ((k1, _), (k2, _)) =>
+          k1 > k2
+      }
+
+    println(s"graph = ${graph}")
+    val scc = fReversed.foldLeft(Vector.empty[Vector[Int]]) {
+      case (components, (_, i)) =>
+        val explored = components.flatten
+        if (explored.contains(i)) components
+        else components :+ dfsStrongComponent(graph, i, i, explored.toSet)
+    }
+    println(s"scc = ${scc}")
+    scc.foreach(println)
+  }
+
+  test("problem") {
+    val inputs = scala.io.Source
+      .fromResource("strong_connected_components.txt")
+      .getLines()
+    val graph = build(inputs.toArray)
+    val ctx = dfsLoop(rev(graph))
+
+    val fReversed: Vector[(Int, Int)] =
+      ctx.f.view.map { case (k, v) => v -> k }.toVector.sortWith {
+        case ((k1, _), (k2, _)) =>
+          k1 > k2
+      }
+
+//    val scc = fReversed.foldLeft(Vector.empty[Vector[Int]]) {
+//      case (components, (_, i)) =>
+//        val explored = components.flatten
+//        if (explored.contains(i)) components
+//        else components :+ dfsStrongComponent(graph, i, i, explored)
+//    }
+
+    val scc = fReversed.foldLeft((Set.empty[Int], Vector.empty[Vector[Int]])) {
+      case ((explored, top5), (_, i)) =>
+        if (explored.contains(i)) (explored, top5)
+        else {
+          val newComponent = dfsStrongComponent(graph, i, i, explored)
+          val top6 = top5 :+ newComponent
+          val nextTop5 = top6.sortWith(_.size > _.size).take(5)
+          (explored ++ newComponent, nextTop5)
+        }
+    }
+
+    println(scc._2.map(_.size))
+//    scc
+//      .map(_.size)
+//      .sortWith(_ > _)
+//      .take(5)
+//      .foreach(println)
   }
 }
