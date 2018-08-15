2f96643c6fa4d0f7c39d10a1b956d99e7ef65e3c
Ikhoon Eom <ih.pert@gmail.com>
2018-08-16 01:18:05 +0900

    coursera(algorithm): weeek 2-2 shortest path

--- scala-exercise/src/main/scala/algonote/Algo_2_2_DijkstraShortestPath.scala
+++ scala-exercise/src/main/scala/algonote/Algo_2_2_DijkstraShortestPath.scala
@@ -3,36 +3,103 @@ package algonote
 import algonote.Algo_2_2_DijkstraShortestPath.{Graph, Node}
 
 import scala.collection.immutable
+import scala.collection.immutable.Queue
 
 /**
   * Created by ikhoon on 06/08/2018.
   */
 class Algo_2_2_DijkstraShortestPath {
 
-  def dijkstraShortestPath(graph: Graph, start: Int): Vector[Int] = {
+  def shortestPath(graph: Graph, start: Int): (Map[Int, Int], Map[Int, Vector[Int]]) = {
     val A = Map(start -> 0)
-    val B = Vector.empty[Int]
+    val B = Map(start -> Vector(1))
 
     val reversed = transpose(graph)
 
-    ???
+    bfs(graph, reversed, A, B, Queue(start), Set.empty)
+
   }
 
-  def transpose(graph: Graph): Map[Int, Vector[Node]] =
+  def bfs(
+    graph: Graph,
+    reversed: Graph,
+    A: Map[Int, Int],
+    B: Map[Int, Vector[Int]],
+    openedSet: Queue[Int],
+    closedSet: Set[Int]
+  ): (Map[Int, Int], Map[Int, Vector[Int]]) = {
+
+    if (openedSet.isEmpty) (A, B)
+    else {
+      val (v, nextQueue) = openedSet.dequeue
+      val adjacent = graph.getOrElse(v, Vector.empty)
+      val newOpenedSet = adjacent
+        .filterNot(node => closedSet.contains(node.vertex))
+        .foldLeft(nextQueue) {
+          case (acc, w) =>
+            if (acc.contains(w.vertex)) acc
+            else acc.enqueue(w.vertex)
+        }
+
+      val (nA, nB, nC, nO) = adjacent
+        .foldLeft((A, B, closedSet, newOpenedSet)) {
+          case ((accA, accB, accC, accO), w) =>
+            val distanceV = accA.getOrElse(v, 0)
+            val distanceVW = distanceV + w.weight
+            val (distance, newB, newC, newO) = accA
+              .get(w.vertex) match {
+              case Some(distanceW) =>
+                if (distanceVW < distanceW) {
+                  val (newC, newO) =
+                    if (accC.contains(w.vertex)) (accC - w.vertex, accO.enqueue(w.vertex))
+                    else (accC, accO)
+                  val pathV = accB.getOrElse(v, Vector.empty)
+                  (distanceVW, accB.updated(w.vertex, pathV :+ w.vertex), newC, newO)
+                } else {
+                  (distanceW, accB, accC, accO)
+                }
+              case None => {
+                val pathV = accB.getOrElse(v, Vector.empty)
+                (distanceVW, accB.updated(w.vertex, pathV :+ w.vertex), accC, accO)
+              }
+            }
+            val newA = accA.updated(w.vertex, distance)
+            (newA, newB, newC, newO)
+        }
+
+      bfs(graph, reversed, nA, nB, nO, nC + v)
+
+    }
+  }
+
+  def transpose(graph: Graph): Graph =
     graph.foldLeft(Map.empty[Int, Vector[Node]]) {
       case (rev, (vertex, adj: Vector[Node])) =>
         adj.foldLeft(rev) {
           case (rev1, edge) =>
-            modify[Int, Vector[Node]](rev1,
-                                      edge.vertex,
-                                      Vector.empty[Node],
-                                      _ :+ Node(vertex, edge.weight))
+            modify[Int, Vector[Node]](rev1, edge.vertex, Vector.empty[Node], _ :+ Node(vertex, edge.weight))
         }
     }
 
   def modify[K, V](map: Map[K, V], key: K, default: V, f: V => V): Map[K, V] = {
     val value = map.getOrElse(key, default)
     map.updated(key, f(value))
+  }
+
+  def build(lines: Array[String]): Graph = {
+    lines.view
+      .map(_.replaceFirst("^\\s+", ""))
+      .filterNot(_.isEmpty)
+      .map(_.split("\\s+"))
+      .map(_.toList)
+      .map {
+        case x :: xs =>
+          (x.toInt, xs.map { y =>
+            val tokens = y.split(",")
+            Node(tokens(0).toInt, tokens(1).toInt)
+          }.toVector)
+      }
+      .toMap
   }
 }
 
--- scala-exercise/src/test/scala/algonote/Algo_2_2_DijkstraShortestPathTest.scala
+++ scala-exercise/src/test/scala/algonote/Algo_2_2_DijkstraShortestPathTest.scala
@@ -22,4 +22,75 @@ class Algo_2_2_DijkstraShortestPathTest extends FunSuite with Matchers {
     val trans = dijkstra.transpose(graph)
     trans.foreach(println)
   }
+
+  /**
+    * 1 - (1) -> 2
+    * |     /   |
+    *(4)  (2)  (6)
+    * |  /      |
+    * ⌄⌞        ⌄
+    * 3 - (3) -> 4
+    */
+  test("simple shortest path") {
+    val graph: Graph = Map(
+      1 -> Vector(Node(2, 1), Node(3, 4)),
+      2 -> Vector(Node(3, 2), Node(4, 6)),
+      3 -> Vector(Node(4, 3)),
+      4 -> Vector.empty
+    )
+
+    val (distance, path) = dijkstra.shortestPath(graph, 1)
+    val expectDistance = Map(
+      1 -> 0,
+      2 -> 1,
+      3 -> 3,
+      4 -> 6
+    )
+    val expectedPath = Map(
+      1 -> Vector(1),
+      2 -> Vector(1, 2),
+      3 -> Vector(1, 2, 3),
+      4 -> Vector(1, 2, 3, 4)
+    )
+
+    distance shouldBe expectDistance
+    path shouldBe expectedPath
+  }
+
+  test("wiki example") {
+    val text =
+      """1 2,4 3,2
+        |2 3,5 4,10
+        |3 5,3
+        |4 6,11
+        |5 4,4
+      """.stripMargin
+    val inputs = text.split("\n")
+    val graph = dijkstra.build(inputs)
+    val (distance, path) = dijkstra.shortestPath(graph, 1)
+    println(distance)
+    println("path")
+    path.foreach(println)
+    path(6) shouldBe Vector(1, 3, 5, 4, 6)
+  }
+
+  test("submit") {
+    val inputs = scala.io.Source
+      .fromResource("dijkstraData.txt")
+      .getLines()
+
+    val graph = dijkstra.build(inputs.toArray)
+    val (distance, path) = dijkstra.shortestPath(graph, 1)
+
+    val destination = List(7, 37, 59, 82, 99, 115, 133, 165, 188, 197)
+
+    val results = destination.map(dest => {
+      distance.getOrElse(dest, 1000000)
+    })
+    println(results)
+    println(results.mkString(","))
+
+    // failed
+    // 3024,3684,2947,2660,2367,2399,3879,2442,2610,5130
+  }
 }
